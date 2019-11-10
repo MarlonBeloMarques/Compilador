d80c833f636d315ad6ebce0051b17202f67aac35
MarlonBeloMarques <marlon.belohd@gmail.com>
2019-11-10 15:12:24 -0300

    Criando operadores logicos

---
+++ Compilador/Linguagem/OOr.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Compilador.Linguagem
+{
+    public class OOr : OLogico, IOperador
+    {
+        #region IOperador Members
+
+        private string _cadeia = "||";
+        public override Cadeia Cadeia
+        {
+            get
+            {
+                return new Cadeia(_cadeia);
+            }
+
+        }
+
+        #endregion
+
+        public OOr()
+        {
+
+        }
+
+        public OOr(int NumeroLinha)
+        {
+            this.Linha = NumeroLinha;
+        }
+    }
+}
