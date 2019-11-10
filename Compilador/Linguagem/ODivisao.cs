1b25cdd3080ec375f6d04c7ef3ce5e87ff117754
MarlonBeloMarques <marlon.belohd@gmail.com>
2019-11-10 15:22:44 -0300

    criando operadores matematicos

---
+++ Compilador/Linguagem/ODivisao.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compilador.Linguagem
+{
+    public class ODivisao : OMatematico, IOperador
+    {
+        #region IOperador Members
+
+        private string _cadeia = @"/";
+        public override Cadeia Cadeia
+        {
+            get
+            {
+                return new Cadeia(_cadeia);
+            }
+        }
+
+        #endregion
+
+        public ODivisao()
+        {
+
+        }
+
+        public ODivisao(int NumeroLinha)
+        {
+            this.Linha = NumeroLinha;
+        }
+    }
+}
---
+++ Compilador/Linguagem/OSoma.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compilador.Linguagem
+{
+    public class OSoma: OMatematico, IOperador
+    {
+        #region IOperador Members
+
+        private string _cadeia = "+";
+        public override Cadeia Cadeia
+        {
+            get
+            {
+                return new Cadeia(_cadeia);
+            }
+        }
+
+        #endregion
+
+        public OSoma()
+        {
+
+        }
+
+        public OSoma(int NumeroLinha)
+        {
+            this.Linha = NumeroLinha;
+        }
+    }
+}
