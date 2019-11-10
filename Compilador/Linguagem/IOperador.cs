91068b410d8f729fcb4754cd46e51133d307ad91
MarlonBeloMarques <marlon.belohd@gmail.com>
2019-11-10 14:36:33 -0300

    Classes necessarias para inicio do desenvolvimento da linguagem

---
+++ Compilador/Linguagem/IOperador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compilador.Linguagem
+{
+    interface IOperador
+    {
+        Cadeia Cadeia
+        {
+            get;
+        }
+    }
+}
---
+++ Compilador/Linguagem/Operador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compilador.Linguagem
+{
+    public abstract class Operador : Token, IOperador
+    {
+        #region IOperator Members
+
+        public abstract Cadeia Cadeia
+        {
+            get;
+        }
+
+        #endregion
+        
+    }
+}
