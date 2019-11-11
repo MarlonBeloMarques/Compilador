1397d2f158a15eedffdeb99a60641d87005c3f18
MarlonBeloMarques <marlon.belohd@gmail.com>
2019-11-11 13:00:00 -0400

    criando analisador lexico

---
+++ Compilador/Linguagem/Variaveis/Variaveis.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compilador.Linguagem.Variaveis
+{
+    public class Variaveis
+    {
+        public Variaveis(List<Valor> Variaveis)
+        {
+            //TODO: CARREGA A LISTA DE VARIAVEIS
+            this._listaVariaveis = Variaveis;
+        }
+
+        public Valor getVariavel(string Nome)
+        {
+            Valor retorno = null;
+            foreach (Valor item in _listaVariaveis)
+            {
+                if (item.NomeVariavel == Nome)
+                {
+                    retorno = item;
+                    break;
+                }
+            }
+            return retorno;
+        }
+
+        private List<Valor> _listaVariaveis;
+        public List<Valor> ListaVariaveis
+        {
+            get
+            {
+                return _listaVariaveis;
+            }
+        }
+
+        public void AdicionarVariavel(Valor Variavel)
+        {
+            _listaVariaveis.Add(Variavel);
+        }
+
+        public bool ExisteVariavel(Valor Variavel)
+        {
+            bool retorno = false;
+
+            foreach (Valor var in _listaVariaveis)
+            {
+                if (var.NomeVariavel == Variavel.NomeVariavel)
+                {
+                    retorno = true;
+                    break;
+                }
+            }
+
+            return retorno;
+        }
+
+        public bool ExisteVariavel(string Variavel)
+        {
+            bool retorno = false;
+
+            Valor vl = new Valor(Variavel, "0", null);
+            retorno = ExisteVariavel(vl);
+
+            return retorno;
+        }
+    }
+}
